48c4ea0e0e52284bc781840a10066a516bccde8e
Phil <pietrask@yahoo.com>
2014-06-19 22:45:14 -0400

    updated for prog ass 2

--- cachematrix.R
+++ cachematrix.R
@@ -7,34 +7,34 @@
 ## cacheSolve(x, ...) returns the inverse of x.  the cached value is used if available
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()  ##get and return inverse of x if already available
-  if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-  }else{   ##otherwise compute inv and cache it
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinv(m)
-    m
-  }
+    m <- x$getinv()                  ##get and return inverse of x if already available
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }else{                           ##otherwise compute inv and cache it
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+    }
 
 }
 
-## makeCacheMatrix() returns an objec that can hold a matrix and cached inverse of the matrix
-##and also the methods to set/get the inverse and set/get the matrix
+## makeCacheMatrix() returns an object that can hold a matrix and cached inverse of the matrix
+## and also the methods to set/get the inverse and set/get the matrix
 ## use x$set() to set the matrix
 ## use x$get() to access the matrix
 ## use x$getinv() to access the inverse if it exists
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL  ## init the inverse
-  set <- function(y) {  ##set the value of matrix and init the inverse
+    m <- NULL                               ## init the inverse
+    set <- function(y) {                    ##set the value of matrix and init the inverse
     x <<- y
     m <<- NULL
-  }
-  get <- function() x  ## access the matrix
-  setinv <- function(solve) m <<- solve   ## set the inverse of the matrix
-  getinv <- function() m  ## access the matrix inverse
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+    }
+    get <- function() x                     ## access the matrix
+    setinv <- function(solve) m <<- solve   ## set the inverse of the matrix
+    getinv <- function() m                  ## access the matrix inverse
+    list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
